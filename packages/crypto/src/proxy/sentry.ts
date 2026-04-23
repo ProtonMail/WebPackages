9c226ca573fe72d449fbc285572f3c0ac95af2ae
Redacted <redacted@invalid.proton.ch>
2026-04-23 17:57:45 +0200

    crypto: fix SentryLogger exported type
    To work with the web-clients logger

--- packages/crypto/src/proxy/sentry.ts
+++ packages/crypto/src/proxy/sentry.ts
@@ -1,5 +1,5 @@
 // we only need type information, so we avoid actually depending on Sentry
 export type SentryLogger = (
     message: string,
-    captureContext?: { level?: "info"; extra?: object },
-) => string;
+    captureContext?: { level?: "info"; extra?: Record<string, unknown> },
+) => unknown; // the official SentryLogger returns a string, but our web-client helper returns void
